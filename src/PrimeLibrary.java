c9ee1dba2e954a52fa145a177df5306978ce9ff2
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2020-12-10 15:13:10 -0800

    Cleaned up the thows clauses, added List support, removed extends Number requirement

--- src/PrimeLibrary.java
+++ src/PrimeLibrary.java
@@ -1,194 +1,247 @@
 package src;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Function;
 
 /*
-NOTE: PARSING INTO STRING IS SUPPORTED!!! 
-Change all istances of <T extends Number> to <T> and <t extends Number> and remove the following:
-
-if (!Number.class.isAssignableFrom(clazz))
-	throw new RuntimeException("Class " + clazz.getName() + " is not valid (must extend java.lang.Number)");
+NOTE: It is recommended that only classes extending Number are used, it is 
+required that the class used has a constructor that accepts a single string argument
 */
-@SuppressWarnings("unchecked")
-public class PrimeLibrary<T extends Number> {
+public class PrimeLibrary<T> {
 
-	final public T[] prime;
+	private static final String DEFAULT_DIRECTORY = System.getProperty("user.dir");
+
+	// Used in calculating big primes.
+	// Better to only create one, and it can be used elsewhere!
+	public static final BigInteger ELEVEN = BigInteger.valueOf(11);
+	public static final BigInteger SEVEN = BigInteger.valueOf(7);
+	public static final BigInteger SIX = BigInteger.valueOf(6);
+	public static final BigInteger FIVE = BigInteger.valueOf(5);
+	public static final BigInteger FOUR = BigInteger.valueOf(4);
+	public static final BigInteger THREE = BigInteger.valueOf(3);
+	public static final BigInteger TWO = BigInteger.valueOf(2);
+	public static final BigInteger ONE = BigInteger.valueOf(1);
+	public static final BigInteger ZERO = BigInteger.valueOf(0);
+
 	/*
 	 * index 0 = 1 so if you think that 1 is trivial, just pretend it is
 	 * one-indexed. When inputting length, it makes an array of length + 1. ie. if
 	 * you want the 50th prime, you need length = 50 and index 50 (prime[50])
 	 */
 
-	private static final String DEFAULT_DIRECTORY = System.getProperty("user.dir");
+	final public T[] prime;
+	public List<T> primeList;
+	// this is so that we can use the functions and consumers
+	private int index = 0;
+	private Scanner recycler;
 
-	public static final BigInteger ELEVEN = BigInteger.valueOf(11);
-	public static final BigInteger SEVEN = BigInteger.valueOf(7);
-	public static final BigInteger SIX = BigInteger.valueOf(6);
-	public static final BigInteger FIVE = BigInteger.valueOf(5);
-	public static final BigInteger FOUR = BigInteger.valueOf(4);
-	public static final BigInteger THREE = BigInteger.valueOf(3);// Used in calculating big primes.
-	public static final BigInteger TWO = BigInteger.valueOf(2);// Better to only create one, and it can be used elsewhere!
-	public static final BigInteger ONE = BigInteger.valueOf(1);
-	public static final BigInteger ZERO = BigInteger.valueOf(0);
-
-	//Some prep here, see buildLibrary for heavy lifting
-	public PrimeLibrary(final int size, final Class<T> clazz) throws IOException,
-			NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException,
-			IllegalArgumentException, InvocationTargetException {
+	// Some prep here, see buildLibrary for heavy lifting
+	public PrimeLibrary(final int size, final Class<T> clazz) throws IllegalArgumentException {
 		this(DEFAULT_DIRECTORY, size, clazz);
 	}
 
 	/*
 	 * Some prep here, see buildLibrary for heavy lifting
 	 */
-	public PrimeLibrary(String DIR, final int size, final Class<T> clazz) throws NoSuchMethodException, SecurityException, IOException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	@SuppressWarnings("unchecked")
+	public PrimeLibrary(String DIR, final int size, final Class<T> clazz) throws IllegalArgumentException {
 		if (size <= 0)
-			throw new IllegalStateException("Library size invalid");
+			throw new IllegalArgumentException("Library size invalid");
 		if (DIR.isEmpty())
 			DIR = DEFAULT_DIRECTORY;
-		if (!Number.class.isAssignableFrom(clazz))
-			throw new RuntimeException("Class " + clazz.getName() + " is not valid (must extend java.lang.Number)");
 		prime = (T[]) Array.newInstance(clazz, size + 1);
-		buildLibrary(DIR, size, clazz.getConstructor(String.class));
+		try {
+			buildLibrary(DIR, size, clazz.getConstructor(String.class));
+		} catch (NoSuchMethodException e) {
+			throw new IllegalArgumentException(
+					"Class " + clazz.getName() + " does not have a constructor that accepts a single string");
+		}
 	}
 
 	/*
-	 * Generates and saves Primes in the CWD
-	 * If the library does not exist, it will be created
-	 * If the library is too large, it will simple read until filled
-	 * If the library is too small, it will recycle the old one to make the new one
+	 * Generates and saves Primes in the CWD If the library does not exist, it will
+	 * be created If the library is too large, it will simple read until filled If
+	 * the library is too small, it will recycle the old one to make the new one
 	 */
-	private void buildLibrary(String DIR, final int length, final Constructor<T> c) throws IOException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	private void buildLibrary(String DIR, final int length, final Constructor<T> c) {
 		final File primeFile = new File(DIR + "\\PrimeLibrary.csv");
-		int index = 0;
-		if (primeFile.exists()) {
-			final Scanner recycler = new Scanner(primeFile);
-			recycler.useDelimiter(",");
-			while (recycler.hasNext()) {
-				prime[index++] = (T) c.newInstance(recycler.next());
-				if (index == prime.length){
-					recycler.close();
-					return;
-				}
-			}
-			System.out.println("I only found " + index + " primes in " + DIR + "\\PrimeLibrary.csv");
-			recycler.close();
-			BigInteger lastNumber = (BigInteger)prime[index - 1];
-			StringBuilder primeStuff = new StringBuilder();
-			for (int i = 0; i < index; i++)
-				primeStuff.append(i).append(",");
-			for (BigInteger i = lastNumber.mod(SIX).compareTo(FIVE) == 0 ? lastNumber.add(SIX) : lastNumber.add(FOUR); index <= length; i = i.add(SIX)) {
-				try {
-					if (isPrime(i)) {
-						prime[index++] = (T) c.newInstance(i.toString());
-						primeStuff.append(i).append(",");
-					}
-					if (isPrime(i.add(TWO))) {
-						if (index < length)
-							prime[index++] = (T) c.newInstance(i.add(TWO).toString());
-						primeStuff.append(i.add(TWO)).append(",");
-					}
-				} catch (NumberFormatException e){
-					System.out.println("WARNING: prime # " + index + " is too large so all instances after this index is null");
-					break; //continue and save data
-				}
-			}
-			try{
-				final FileWriter primeWriter = new FileWriter(primeFile);
-				primeWriter.append(primeStuff.toString());
-				primeWriter.close();
-			}catch (Exception e){
-			}
-			return;
-		}
-		if (length > 4){
-			prime[4] = (T) c.newInstance("7");
-			prime[3] = (T) c.newInstance("5");
-			prime[2] = (T) c.newInstance("3");
-			prime[1] = (T) c.newInstance("2");
-			prime[0] = (T) c.newInstance("1");
-		}else
-			switch(length){
-				case 4: prime[4] = (T) c.newInstance("7");
-				case 3: prime[3] = (T) c.newInstance("5");
-				case 2: prime[2] = (T) c.newInstance("3");
-				case 1: prime[1] = (T) c.newInstance("2"); prime[0] = (T) c.newInstance("1"); return;
-				default: throw new RuntimeException("How the hell did I miss that? bad lib size");
-			}
 		StringBuilder primeText = new StringBuilder();
-		primeText.append("1,2,3,5,7,");
-		index = 5;
-		for (BigInteger i = ELEVEN; index <= length; i = i.add(SIX)) {
-			try{
-				if (isPrime(i)) {
-					prime[index++] = (T) c.newInstance(i.toString());
-					primeText.append(i).append(",");
+		recycler = null;
+
+		// This is how we save data
+		Function<Boolean, Boolean> quitAndSave = (val) -> {
+			try {
+				primeFile.createNewFile();
+				final FileWriter primeWriter = new FileWriter(primeFile);
+				primeWriter.append(primeText.toString());
+				primeWriter.close();
+				if (recycler != null)
+					recycler.close();
+			} catch (IOException ignoredIOException) {
+				System.out.println("AN ERROR OCURRED USING " + DIR
+						+ "\\PrimeLibrary.csv!!! This will only affect the runtime length because PrimeNumberLibrary will have to recalculate all of the primes instead of picking up where it left off");
+			}
+			return val;
+		};
+
+		// This creates the number from the string
+		Function<String, T> constructor = numberIn -> {
+			try {
+				return (T) c.newInstance(numberIn);
+			} catch (InstantiationException | IllegalArgumentException | InvocationTargetException
+					| IllegalAccessException e) {
+				e.printStackTrace();
+				throw new NumberFormatException("Something went wrong constructing the object (maybe overflow)");
+			}
+		};
+
+		// This both makes and assigns the number in a wrapped way
+		Function<String, Boolean> assignNewNumber = numberIn -> {
+			try {
+				this.prime[this.index++] = constructor.apply(numberIn);
+				primeText.append(numberIn).append(",");
+			} catch (NumberFormatException | IndexOutOfBoundsException e) {
+				return quitAndSave.apply(Boolean.valueOf(true));
+			}
+			return this.index == prime.length ? quitAndSave.apply(Boolean.valueOf(true)) : false;
+		};
+
+		if (primeFile.exists()) {
+			try {
+				recycler = new Scanner(primeFile);
+			} catch (FileNotFoundException fnfe) {
+				// this should never happen but if it does we are boned
+				fnfe.printStackTrace();
+				throw new RuntimeException("Something went horribly wrong I think im getting trolled");
+			}
+			recycler.useDelimiter(",");
+			while (recycler.hasNext())
+				if (assignNewNumber.apply(recycler.next()))
+					return;
+			System.out.println("I only found " + this.index + " primes in " + DIR + "\\PrimeLibrary.csv");
+			BigInteger lastNumber = BigInteger.valueOf(Long.parseLong("" + prime[this.index - 1]));
+			for (BigInteger i = lastNumber.mod(SIX).compareTo(FIVE) == 0 ? lastNumber.add(SIX)
+					: lastNumber.add(FOUR); this.index <= length; i = i.add(SIX)) {
+				if (isPrime(i))
+					if (assignNewNumber.apply(i.toString()))
+						return;
+				if (isPrime(i.add(TWO)))
+					if (assignNewNumber.apply(i.add(TWO).toString()))
+						return;
+			}
+		} else {
+			if (length > 4) {
+				if (assignNewNumber.apply("1") || assignNewNumber.apply("2") || assignNewNumber.apply("3")
+						|| assignNewNumber.apply("5") || assignNewNumber.apply("7"))
+					return;
+			} else
+				switch (length) {
+					case 4:
+						prime[4] = constructor.apply("7");
+					case 3:
+						prime[3] = constructor.apply("5");
+					case 2:
+						prime[2] = constructor.apply("3");
+					case 1:
+						prime[1] = constructor.apply("2");
+						prime[0] = constructor.apply("1");
+						quitAndSave.apply(null);
+						return;
+					default:
+						throw new RuntimeException("How the hell did I miss that? bad lib size");
 				}
-				if (isPrime(i.add(TWO))) {
-					if (index < length)
-						prime[index++] = (T) c.newInstance(i.add(TWO).toString());
-					primeText.append(i.add(TWO)).append(",");
-				}
-			} catch (NumberFormatException e){
-				System.out.println("WARNING: prime # " + index + " is too large so all instances after this index is null");
-				break; //continue and save data
+			for (BigInteger i = ELEVEN; this.index <= length; i = i.add(SIX)) {
+				if (isPrime(i))
+					if (assignNewNumber.apply(i.toString()))
+						return;
+				if (isPrime(i.add(TWO)))
+					if (assignNewNumber.apply(i.add(TWO).toString()))
+						return;
 			}
 		}
-		try{
-			primeFile.createNewFile();
-			final FileWriter primeWriter = new FileWriter(primeFile);
-			primeWriter.append(primeText.toString());
-			primeWriter.close();
-		}catch (IOException e){
-			System.out.println("AN ERROR OCURRED USING " + DIR + "\\PrimeLibrary.csv!!! This will only affect the runtime length because PrimeNumberLibrary will have to recalculate all of the primes instead of picking up where it left off");
-		}
+		quitAndSave.apply(null);
 	}
 
-	//This will make a new instance. for example: PrimeLibrary<Integer> lib = PrimeLibrary.createLibrary("", 50, Integer.class);
-	public static <t extends Number> PrimeLibrary<t> createLibrary(final String DIR, final int length, final Class<t> clazz) throws IOException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	// This will make a new instance. for example: PrimeLibrary<Integer> lib =
+	// PrimeLibrary.createLibrary("", 50, Integer.class);
+	public static <t extends Number> PrimeLibrary<t> createLibrary(final String DIR, final int length,
+			final Class<t> clazz) throws IllegalArgumentException {
 		return new PrimeLibrary<t>(DIR, length, clazz);
 	}
 
-	//This will make a new instance. for example: PrimeLibrary<Integer> lib = PrimeLibrary.createLibrary(50, Integer.class);
-	public static <t extends Number> PrimeLibrary<t> createLibrary(final int length, final Class<t> clazz) throws IOException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	// This will make a new instance. for example: PrimeLibrary<Integer> lib =
+	// PrimeLibrary.createLibrary(50, Integer.class);
+	public static <t extends Number> PrimeLibrary<t> createLibrary(final int length, final Class<t> clazz)
+			throws IllegalArgumentException {
 		return new PrimeLibrary<t>(length, clazz);
+	}
+
+	//Recreates the list
+	public List<T> remakeList(){
+		this.primeList = Arrays.asList(this.prime);
+		return this.primeList;
+	}
+
+	//Creates the list
+	public PrimeLibrary<T> makeList(){
+		if (this.primeList == null)
+			this.primeList = Arrays.asList(this.prime);
+		return this;
+	}
+
+	//Not recommended, but its here
+	public T get(int index) throws IndexOutOfBoundsException{
+		return this.prime[index];
+	}
+
+	//Identical to getList()
+	public List<T> get(){
+		return this.remakeList();
+	}
+
+	//Identical to get()
+	public List<T> getList(){
+		return this.remakeList();
 	}
 
 	/*
 	 * Nothing revolutionary. True = argument is prime
 	 */
-	private static boolean isPrime(final BigInteger sum) throws IllegalArgumentException {
-		if (sum.mod(FIVE).compareTo(ZERO) == 0) 
+	private static boolean isPrime(final BigInteger sum) {
+		if (sum.mod(FIVE).compareTo(ZERO) == 0)
 			return false;
-		if (sum.mod(SEVEN).compareTo(ZERO) == 0) 
+		if (sum.mod(SEVEN).compareTo(ZERO) == 0)
 			return false;
 		final BigInteger goal = bsqrt(sum);
-		if (goal.multiply(goal).compareTo(sum) == 0)//if perfect square, go home
+		if (goal.multiply(goal).compareTo(sum) == 0)// if perfect square, go home
 			return false;
-		for (BigInteger i = ELEVEN; goal.compareTo(i) >= 0 && i.compareTo(sum) != 0; i = i.add(SIX)) 
-			if (sum.mod(i).compareTo(ZERO) == 0 || sum.mod(i.add(TWO)).compareTo(ZERO) == 0) 
+		for (BigInteger i = ELEVEN; goal.compareTo(i) >= 0 && i.compareTo(sum) != 0; i = i.add(SIX))
+			if (sum.mod(i).compareTo(ZERO) == 0 || sum.mod(i.add(TWO)).compareTo(ZERO) == 0)
 				return false;
 		return true;
 	}
 
-	//BigInteger has a native sqrt function is VS code but in command line no so heres the manual one that will ensure the limiting factor is your computer's specs
-	private static BigInteger bsqrt(final BigInteger x) throws IllegalArgumentException {
+	// BigInteger has a native sqrt function is VS code but in command line no so
+	// heres the manual one that will ensure the limiting factor is your computer's
+	// specs
+	private static BigInteger bsqrt(final BigInteger x) {
 		if (x.compareTo(BigInteger.ZERO) < 0)
 			throw new IllegalArgumentException("Negative argument.");
-		// square roots of 0 and 1 are trivial and
-		// y == 0 will cause a divide-by-zero exception
 		if (x == BigInteger.ZERO || x == BigInteger.ONE)
 			return x;
 		BigInteger y;
-		// starting with y = x / 2 avoids magnitude issues with x squared
-		for (y = x.divide(TWO); y.compareTo(x.divide(y)) > 0; y = ((x.divide(y)).add(y)).divide(TWO));
+		for (y = x.divide(TWO); y.compareTo(x.divide(y)) > 0; y = ((x.divide(y)).add(y)).divide(TWO))
+			;
 		if (x.compareTo(y.multiply(y)) == 0)
 			return y;
 		else
--- src/PrimeLibraryRunner.java
+++ src/PrimeLibraryRunner.java
@@ -8,13 +8,13 @@ class PrimeLibraryRunner{
 		final long timeIn = System.currentTimeMillis();
 		//You can omit the second <BigInteger> but it gets verbose fast
 		//PrimeLibrary<BigInteger> lib = new PrimeLibrary<BigInteger>(libSize, BigInteger.class);
-		PrimeLibrary<BigInteger> lib = PrimeLibrary.createLibrary("", 50, BigInteger.class);
+		PrimeLibrary<BigInteger> lib = PrimeLibrary.createLibrary("", libSize, BigInteger.class);
 		System.out.println("Results for lib of size " + libSize);
 		System.out.println("Runtime time: " + (System.currentTimeMillis() - timeIn) + "ms");
 		System.out.println("Average time: " + ((System.currentTimeMillis() - timeIn) / (double)libSize) + "ms");
 		try{
 			System.out.println(lib.prime[80]);
-			System.out.println(lib.prime[1080]);
+			System.out.println(lib.get(1080));
 			System.out.println(lib.prime[2080]);
 			System.out.println(lib.prime[3080]);
 			System.out.println(lib.prime[4080]);
